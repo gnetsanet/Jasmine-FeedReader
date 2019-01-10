3df42504372b03202bf2dc22319d451554a92f15
Netsanet Gebremedhin <gnetsanet@gmail.com>
2019-01-10 10:15:51 -0500

    Test there is at least one entry in all feeds.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -76,20 +76,21 @@ $(function() {
         });
     });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+    describe('Initial Entries', function() {
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        //Jasmine's beforeEach
+        beforeEach(function(done) {
+            loadFeed(0,done);
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        // Check if feeds have been loaded by checking
+        // there is at least one .entry class in .feed container
+       
+        it('Entry in a feed', function(done) {
+            expect(document.body.getElementsByClassName('entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
